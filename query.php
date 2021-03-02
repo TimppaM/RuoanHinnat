bf2f44ba64f31981506e02384c7207fc97be2dcb
Timo Mykkänen <77782555+TimppaM@users.noreply.github.com>
2021-03-02 18:54:54 +0200

    JQuery, Ajax update.

---
+++ query.php
@@ -0,0 +1,44 @@
+
+<?php
+// Ruoan hintojen tarkistaminen foodie.fi-sivustoa parsettamalla
+// Timo Mykkänen 2021
+// PHP Backend
+header('Access-Control-Allow-Origin: *');
+header('Content-type: application/json');
+
+
+if (!empty($_GET['tuotenimi']))
+ {
+    // Korvataan välilyönnit %20, jotta haku toimii oikein.
+    $noSpacestuotenimi = str_replace(' ', '%20', $_GET['tuotenimi']);
+    $data = file_get_contents("https://www.foodie.fi/products/search2?term=".$noSpacestuotenimi);
+
+    // Foodie.fi-sivustolta parsetetaan json-dataa, jossa on ylempää hakuehtoa vastaavat tulokset.
+    // Pitää hiukan manuaalisesti muokkailla ja trimmata palautuvaa objectia, jotta saadaan data oikeanlaiseen JSON-muotoon.
+	if (preg_match('/(page.init)(.*?)("pagination)/', $data, $matches)) {
+
+	$str_new = ltrim($matches[2], '(');
+	$str_new = rtrim($str_new, ', ');
+	$str_new = $str_new . "}";
+	
+	$json = json_decode($str_new, true);
+
+// Haetaan json-arraysta rivi "hinta", jotta voidaan myöhemmin lajitella tuotteet hinnan mukaan.                                                                                                                                                           
+foreach ($json["entries"] as $key => $row)
+{
+    $tuotteenhinta[$key]  = $row['price'];
+}    
+
+// Palautetaan hakua vastaavat tulokset, jos niitä löytyi enemmän kuin yksi ja lajitellaan hinnan mukaan pienimmästä suurimpaan.
+if(count($json["entries"]) > 1)
+{
+array_multisort($tuotteenhinta, SORT_ASC, $json["entries"]);
+}
+
+}
+}
+
+// Palautetaan fronttipuolelle json-array.
+echo json_encode($json["entries"]); 
+
+?>
